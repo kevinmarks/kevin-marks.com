eb46fb2d5309af59e21f0f59800f50a3a5c4ee5f
Kevin Marks <kevinmarks@gmail.com>
2013-09-06 04:01:36 -0700

    retro look

---
+++ web.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+app.configure(function(){
+    app.use(express.static(__dirname + '/web'));
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
