2d3fa83b162525b5238665843d3878497c573ead
Kevin Marks <kevinmarks@gmail.com>
2016-09-06 11:22:54 -0700

    switch to inline SVG checkmarks

--- web/verify.js
+++ web/verify.js
@@ -17,9 +17,9 @@
                 data: {url1: url, url2: relMeUrl}
             }).done(function(data) {
                 if (data === 'true') {
-                    el.addClass("verified");
+                    el.addClass("verified").append('<svg xmlns="http://www.w3.org/2000/svg" width="1em" style="vertical-align:text-bottom" viewbox="0 0 40 40"><circle cx="20" cy="20" r="18" fill="green" stroke="lightgreen" stroke-width="3"/><path d="M 10,20 18,28 33,14" fill="none" stroke="white" stroke-width="6"/></svg>');
                 } else {
-                    el.addClass("unverified");
+                    el.addClass("unverified").append('<svg xmlns="http://www.w3.org/2000/svg" width="1em" style="vertical-align:text-bottom"  viewbox="0 0 40 40"><circle cx="20" cy="20" r="18" fill="darkred" stroke="red" stroke-width="3"/><path d="M 10,10 30,30 M 10,30 30,10" fill="none" stroke="white" stroke-width="6"/></svg>');
                 }
             }).fail(function(xhr) {
                 console.log('HTTP request failed:', xhr);
