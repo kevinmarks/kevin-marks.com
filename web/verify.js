268285cf9b9fb04f6738735e491690cfdf00888c
Kevin Marks <kevinmarks@gmail.com>
2016-09-05 18:52:03 -0700

    Add rel-me verification
    uses indiewebify.me service

--- web/verify.js
+++ web/verify.js
@@ -1,4 +1,29 @@
-var links = document.querySelectorAll('a');
-for (i = 0; i < links.length; ++i) {
-  console.log(links[i].href);
-}
+
+// rel-me checking via indiewebify.me
+(function($) {
+    var results = $('a[rel~="me"]'),
+        url =  window.location.href,
+        canon = $("link[rel~=canonical]");
+    if (canon[0]){
+        url = canon[0].href;
+    }
+    results.each(function() {
+        var relMeUrl = this.href,
+            el = $(this);
+        console.log(url, relMeUrl);
+        // validate symmetric rels with request to /rel-me-links/
+        if (relMeUrl.startsWith('http')) {
+            $.ajax('http://indiewebify.me/rel-me-links/', {
+                data: {url1: url, url2: relMeUrl}
+            }).done(function(data) {
+                if (data === 'true') {
+                    el.addClass("verified");
+                } else {
+                    el.addClass("unverified");
+                }
+            }).fail(function(xhr) {
+                console.log('HTTP request failed:', xhr);
+            });
+        }
+    });
+}($));
