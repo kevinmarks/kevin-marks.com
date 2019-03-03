d58239c36fa5414859c9f5513368fa9c913e5a9f
Kevin Marks <kevin.marks.ext@bbc.co.uk>
2019-03-03 14:57:19 +0000

    Twitter player test

--- web/tweets/data/js/user_details.js
+++ web/tweets/data/js/user_details.js
@@ -1,6 +1,6 @@
 var user_details =  {
   "screen_name" : "kevinmarks",
-  "location" : "Nunthorpe, England",
+  "location" : "Portland, OR",
   "full_name" : "Kevin Marks",
   "bio" : "\uD83D\uDCD6Reading your thoughts. \u270D\uFE0FIf you write them first. \uD83D\uDD78https:\/\/t.co\/LD3HeubE2z Want to hire me to consult, write or speak? \uD83D\uDCE9dm me",
   "id" : "57203",
